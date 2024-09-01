f3d9df03849b8962613ebb0de1fbd4de00eba3d6
DannyP39 <danipiza@ucm.es>
2024-09-01 23:02:22 +0200

    Lunar Lander parameters search

---
+++ Games/Gym/p1.py
@@ -0,0 +1,7 @@
+fc_dim, eps_dec, lr, avg_score, done, time= 64, 0.01, 0.001, 100.1921, False, 5234.124
+
+message = 'fc_dim: {}, eps_dec: {}, lr: {}, avg_score: {:.2f}, done: {}, time: {:.2f}'.format(
+    fc_dim, eps_dec, lr, avg_score, done, time
+)
+
+print(message)
